1731a24a42c283b4f4742266f6a8fb1c9135ada4
istwoodru <istwood123@yandex.ru>
2024-09-07 00:46:09 +0700

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,5 @@
+print(' "4th program" ')
+print('123.456')
+print(type(float('123.456')))
+print(int(123.456 * 10.0) % 10)
+
---
+++ variable.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+spending_hours = 1.5
+name_of_course ="Python"
+time_on_1task = (spending_hours / number_of_homework)
+print("Курс:",name_of_course,",всего задач:",number_of_homework,",затрачено часов:",spending_hours,",среднее время выполнения",time_on_1task,"часа")
